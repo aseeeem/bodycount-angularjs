c1ed08957105ce0f6b5ad0afc4a8099cb9af134c
Asim Shamim <shamima@wit.edu>
2016-08-05 09:24:56 -0400

    pushing laptop files

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+   selector: 'app',
+   template: '<h1>BodyCount</h1>'
+})
+// Adding export statement so it can be used elsewhere
+export class AppComponent {
+   constructor() {
+
+   }
+}
