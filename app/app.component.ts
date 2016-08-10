24a18d0e68fae499cef69e148c489db88609cb43
Asim Shamim <shamima@wit.edu>
2016-08-10 00:59:13 -0400

    Updated app.component.ts with more comments
    In the event that this code gets used somewhere else, more comments were added.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,8 @@
-// Imports component decarator
+/* 
+app.component.ts 
+for Asim Shamim's Senior Design Project
+written for Angular2
+*/
 import {Component} from '@angular/core';
 import {Response, Http} from '@angular/http';
 import 'rxjs/Rx';
@@ -13,6 +17,18 @@ export class AppComponent {
   public title = "Here's how many people we've seen";
   public footer_details = "Wentworth 2016";
   constructor(private _http: Http) {  }
+  /* 
+  Async magic happens here
+  Creates observable object 'counts'
+  which refreshes at some interval (in our case 2 seconds)
+  All of that is eventually mapped from a json file to counts
+  */
+  
+  /* 
+  The 'countJSONdata.json' file would be modified at some undefined interval
+  For demonstration purposes, I set the interval to 2 seconds
+  This file would be updated with the new count and time data
+  */
   counts = Observable.interval(2000)
   .switchMap(() => this._http.get("app/countJSONdata.json"))
   .map((res: Response) => res.json().count);
